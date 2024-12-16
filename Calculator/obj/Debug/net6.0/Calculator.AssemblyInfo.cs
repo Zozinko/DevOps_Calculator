93fb7215e0d2d91e918e8c05686e260101b72c4f
Zozinko <kolyamr16@gmail.com>
2024-12-16 15:29:40 +0500

    laba 1 create 2
    create laba1

--- Calculator/obj/Debug/net6.0/Calculator.AssemblyInfo.cs
+++ Calculator/obj/Debug/net6.0/Calculator.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf11526cae9e05984eda308781d1c55759de6467")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
